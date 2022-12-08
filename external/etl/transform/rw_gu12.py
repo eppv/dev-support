a57b1925befcc368c139c188889f121893973a13
eppv <evgeniypalych@gmail.com>
2022-12-08 11:03:22 +0700

    external modules: added custom transforming rename logic to rw_gu12 module

--- external/etl/transform/rw_gu12.py
+++ external/etl/transform/rw_gu12.py
@@ -8,10 +8,20 @@ def define_headers(df, search_col_name, search_col_idx=0):
     return df
 
 
+def rename_col(col_name):
+    import re
+    if re.match(r'([А-Я][а-я]+\S[а-я]+\s[а-я]+\.)', col_name):
+        new_col_name = re.sub('[А-Я][а-я]+\S[а-я]+\s[а-я]+\.\/[а-я]+\.', 'Кол-во ваг.', col_name)
+        return new_col_name
+    else:
+        return col_name
+
+
 def transform(df, config):
     column_name = config['column_name']
     columns_list = config['columns_list']
     df_with_headers = define_headers(df, search_col_name=column_name)
+    df_with_headers.rename(columns=rename_col, inplace=True)
     df_with_headers = df_with_headers[columns_list]
     df_with_headers.dropna(thresh=3, inplace=True)
 
