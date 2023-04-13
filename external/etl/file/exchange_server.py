3c6ab264be37e6f967e8564f1902139d8f6cb216
eppv <evgeniypalych@gmail.com>
2023-04-13 12:03:11 +0700

    external modules: to exchange_server etl modules added new features for metadata capture, to utils.var added funcs to extract datetime from path

--- external/etl/file/exchange_server.py
+++ external/etl/file/exchange_server.py
@@ -7,6 +7,9 @@ from exchangelib import FileAttachment
 from datetime import datetime
 from pytz import timezone
 
+LOCAL_TZ_NAME = os.getenv('AIRFLOW__CORE__DEFAULT_TIMEZONE')
+LOCAL_TZ = timezone(LOCAL_TZ_NAME)
+
 def get_items(account, folder, received_start, received_end):
     inbox = account.inbox
     src_folder = inbox.glob(f'**/{folder}')
@@ -20,15 +23,29 @@ def get_items(account, folder, received_start, received_end):
 
     return items
 
-
 def get_excel_attachments(item):
     excel_attachments = []
     for attachment in item.attachments:
         if isinstance(attachment, FileAttachment) and attachment.name.endswith(('.xlsx', '.xls')):
             print(f'Attachment: {attachment.name}')
             excel_attachments.append(attachment)
-            return excel_attachments
+            item_meta = {
+                'item_subject': item.subject,
+                'item_received': to_datetime_string(item.datetime_received.astimezone(LOCAL_TZ))
+            }
 
+            return excel_attachments, item_meta
+
+
+def get_attachments_to_extract_with_meta(messages: list):
+    attachments_with_meta_to_extract = {}
+    for msg in messages:
+        attachments, item_meta = get_excel_attachments(msg)
+        for attachment in attachments:
+            attachment_with_meta = {attachment: item_meta}
+            attachments_with_meta_to_extract.update(attachment_with_meta)
+
+    return attachments_with_meta_to_extract
 
 def excel_extract_from_attachment(attachment, config, transform=None):
     from witness import Batch
@@ -41,9 +58,9 @@ def excel_extract_from_attachment(attachment, config, transform=None):
     record_source_array = [src_config['mail']['server'],
                            src_config['mail']['folder'],
                            attachment.name,
-                           to_datetime_string(attachment.last_modified_time)]
+                           to_datetime_string(attachment.last_modified_time.astimezone(LOCAL_TZ))]
 
-    print(attachment.last_modified_time)
+    print(attachment.last_modified_time.astimezone(LOCAL_TZ))
 
     extractor = PandasExcelExtractor(uri=attachment.content,
                                      sheet_name=sheet_name,
@@ -77,33 +94,24 @@ def extract(config, transform=None):
                              login=src_config['mail']['login'],
                              password=src_config['mail']['password'])
     target_folder = src_config['mail']['folder']
-    local_tz_name = os.getenv('AIRFLOW__CORE__DEFAULT_TIMEZONE')
-    local_tz = timezone(local_tz_name)
-
-    # received_start = src_config['mail']['received']['start']
-    # received_end = src_config['mail']['received']['end']
-
     received_start = src_config['mail']['received']['start']
     received_end = src_config['mail']['received']['end']
 
-    received_start_dt = datetime.fromisoformat(received_start).astimezone(local_tz)
-    received_end_dt = datetime.fromisoformat(received_end).astimezone(local_tz)
-    print(f'String start: {received_start},tz aware dt: {received_start_dt}')
+    received_start_dt = datetime.fromisoformat(received_start).astimezone(LOCAL_TZ)
+    received_end_dt = datetime.fromisoformat(received_end).astimezone(LOCAL_TZ)
+    print(f'Received Start Datetime: {received_start}, Received Start Datetime TZ Aware: {received_start_dt}')
 
     messages = get_items(my_account,
                          folder=target_folder,
                          received_start=received_start_dt,
                          received_end=received_end_dt)
 
-    attachments_to_extract = []
-    for msg in messages:
-        attachments_to_extract.extend(get_excel_attachments(msg))
+    attachments_to_extract_with_meta = get_attachments_to_extract_with_meta(messages)
 
-
-    extracted = []
-
-    for attachment in attachments_to_extract:
+    extracted_meta = []
+    for attachment, item_meta in attachments_to_extract_with_meta.items():
         meta = excel_extract_from_attachment(attachment, config, transform=transform)
-        extracted.append(meta)
+        meta.update(item_meta)
+        extracted_meta.append(meta)
 
-    return extracted
+    return extracted_meta
--- external/utils/var.py
+++ external/utils/var.py
@@ -1,5 +1,6 @@
 import datetime
 import pendulum
+import os
 
 
 def params_to_str(http_params: dict) -> str:
@@ -44,3 +45,41 @@ def to_datetime_string(dt):
         dt_str = dt.strftime('%Y-%m-%d %H-%M-%S')
         return dt_str
     else: return dt
+
+
+import re
+
+def extract_datetime_string_from_filename(filename):
+    # define regular expression patterns to match datetime strings in filename
+    datetime_regexes = [
+        r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',  # YYYY-MM-DD HH:MM:SS
+        r'\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}',  # YYYY-MM-DD_HH-MM-SS
+        r'\d{4}\d{2}\d{2}_\d{2}\d{2}\d{2}',  # YYYYMMDD_HHMMSS
+        r'\d{4}\d{2}\d{2}_\d{2}\d{2}',  # YYYYMMDD_HHMM
+        r'\d{4}-\d{2}-\d{2}',  # YYYY-MM-DD
+        r'\d{4}\d{2}\d{2}',  # YYYYMMDD
+    ]
+
+    # search for datetime string in filename using each regex pattern
+    for regex in datetime_regexes:
+        match = re.search(regex, filename)
+        if match:
+            # extract datetime string from regex match
+            datetime_str = match.group(0)
+            return datetime_str
+
+    # return None if no datetime string found in filename
+    return None
+
+def extract_datetime_from_path(path):
+    # extract filename from path
+    filename = os.path.basename(path)
+
+    # extract datetime string from filename using regex or string manipulation
+    datetime_str = extract_datetime_string_from_filename(filename)
+
+    # convert datetime string to datetime object
+    datetime_obj = datetime.datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+
+    return datetime_obj
+
